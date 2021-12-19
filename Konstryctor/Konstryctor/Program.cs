427ee505c1e107e8d1f8be835ab2f4a85581396c
Galinka <galinka2002niko@gmail.com>
2021-12-20 00:12:21 +0300

    ert

---
+++ Konstryctor/Konstryctor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Konstryktor
+{
+    class MyClass
+    {
+        public string Name;
+        public byte Age;
+        // Создаем параметрический конструктор
+        public MyClass(string s, byte b)
+        {
+            Name = s;
+            Age = b;
+        }
+        public void reWrite()
+        {
+            Console.WriteLine("Имя: {0}\nВозраст: {1}", Name, Age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass ex = new MyClass("Alexandr", 26);
+            ex.reWrite();
+            Console.ReadLine();
+        }
+    }
+}
